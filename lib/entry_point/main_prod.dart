a6f287a558b4e27980afc0d150100b5485a9aacd
Azlaan Khan <azlaan95@gmail.com>
2024-06-18 21:38:19 +0530

    Place your Test token in .env.dev under Environment module

--- lib/entry_point/main_prod.dart
+++ lib/entry_point/main_prod.dart
@@ -1,13 +1,9 @@
 import 'package:kanban_taskmanager/di/AppConfigure.dart';
 import 'package:kanban_taskmanager/environment/development/development_env.dart';
-import 'my_app.dart';
-import 'package:azl_data/config/app_config.dart';
 
-//Todo: Place you Test Token here
-const String testToken = "";
+import 'my_app.dart';
 
 Future<void> main() async {
   AppConfigure.configure(DevelopmentEnvironment.development());
-  AppConfig.shared.token = "Bearer $testToken";
   await myMain();
 }
